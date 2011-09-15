2abd0a7caaaa114303e17fcc32f65b42736735e5
bargool <bargool@gmail.com>
2011-09-15 18:42:16 +0400

    AddLinesToGroup and GetLinesGroup work fine (first working version), but they are need refactoring GetLinesGroup writes out all group lengths in command string

--- Electro/ElectroLines.cs
+++ Electro/ElectroLines.cs
@@ -16,6 +16,8 @@ namespace Electro
 {
 	public class ElectroLines
 	{
+		const string AppRecordKey = "BargElectroLinesGroup";
+		
 		[CommandMethod("AddLinesToGroup")]
 		public void AddLinesToGroup()
 		{
@@ -28,8 +30,7 @@ namespace Electro
 				pStringOptions.AllowSpaces = false;
 				pStringOptions.DefaultValue = "Гр.1";
 				PromptResult pStringResult = acadDocument.Editor.GetString(pStringOptions);
-				string GroupName = pStringResult.StringResult;
-				//Application.ShowAlertDialog("Наименование группы: " + pStringResult.StringResult);
+				string GroupName = pStringResult.StringResult; //Имя группы
 				PromptSelectionOptions acadSelectionOptions = new PromptSelectionOptions();
 				acadSelectionOptions.MessageForAdding = "\nУкажите объекты группы";
 				//Выделять будем только линии и полилинии. Создаем фильтр
@@ -52,10 +53,9 @@ namespace Electro
 							Entity entity = acadTrans.GetObject(selectedObj.ObjectId, OpenMode.ForWrite) as Entity;
 							if (entity!=null)
 							{
-								string RecordKey = "BargElectroLines";
-								TypedValue[] data = new TypedValue[2];
-								data.SetValue(new TypedValue((int)DxfCode.Text, "GroupLines"),0);
-								data.SetValue(new TypedValue((int)DxfCode.Text, GroupName),1);
+								TypedValue data = new TypedValue((int)DxfCode.Text, GroupName);
+								//data.SetValue(new TypedValue((int)DxfCode.Text, "GroupLine"),0);
+								//data .SetValue (new TypedValue((int)DxfCode.Text, GroupName),0);
 								ResultBuffer buffer = new ResultBuffer(data);
 								
 								if (entity.ExtensionDictionary==ObjectId.Null)
@@ -65,17 +65,22 @@ namespace Electro
 								using (DBDictionary dict = acadTrans.GetObject(
 									entity.ExtensionDictionary, OpenMode.ForWrite, false) as DBDictionary)
 								{
-									if (dict.Contains(RecordKey))
+									if (dict.Contains(AppRecordKey))
 									{
 										Xrecord xrecord = acadTrans.GetObject(
-											dict.GetAt(RecordKey), OpenMode.ForWrite) as Xrecord;
-										xrecord.Data = buffer;
+											dict.GetAt(AppRecordKey), OpenMode.ForWrite) as Xrecord;
+										if (!xrecord.Data.AsArray().Contains(data))
+										{
+											buffer = xrecord.Data;
+											buffer.Add(data);
+											xrecord.Data = buffer;
+										}
 									}
 									else
 									{
 										Xrecord xrecord = new Xrecord();
 										xrecord.Data = buffer;
-										dict.SetAt(RecordKey, xrecord);
+										dict.SetAt(AppRecordKey, xrecord);
 										acadTrans.AddNewlyCreatedDBObject(xrecord, true);
 									}
 								}
@@ -84,6 +89,106 @@ namespace Electro
 					}
 				}
 				acadTrans.Commit();
+			}
+		}
+		
+		[CommandMethod("GetLinesroup")]
+		public void GetLinesGroup()
+		{
+			//Dictionary, в котором будут храниться номера групп и длины
+			Dictionary<string, double> GroupLenghts = new Dictionary<string, double>();
+			
+			Document acadDocument = Application.DocumentManager.MdiActiveDocument;
+			Database acadCurDb = acadDocument.Database;
+			using (Transaction acadTrans = acadCurDb.TransactionManager.StartTransaction())
+			{
+				//Спрашиваем имя группы
+//				PromptStringOptions pStringOptions = new PromptStringOptions("\nВведите наименование группы: ");
+//				pStringOptions.AllowSpaces = false;
+//				pStringOptions.DefaultValue = "Гр.1";
+//				PromptResult pStringResult = acadDocument.Editor.GetString(pStringOptions);
+//				string GroupName = pStringResult.StringResult; //Имя группы
+				//Выделять будем только линии и полилинии. Создаем фильтр
+				TypedValue[] acadFilterValues = new TypedValue[4];
+				acadFilterValues.SetValue(new TypedValue((int)DxfCode.Operator, "<OR"),0);
+				acadFilterValues.SetValue(new TypedValue((int)DxfCode.Start, "LINE"),1);
+				acadFilterValues.SetValue(new TypedValue((int)DxfCode.Start, "LWPOLYLINE"),2);
+				acadFilterValues.SetValue(new TypedValue((int)DxfCode.Operator, "OR>"),3);
+				SelectionFilter acadSelFilter = new SelectionFilter(acadFilterValues);
+				//Выделяем ВСЕ линии и полилинии на незамороженных слоях
+				SelectionSet selectionSet = acadDocument.Editor.SelectAll(acadSelFilter).Value;
+				//Проходим по получившемуся выделению
+				foreach (SelectedObject selectedObj in selectionSet)
+				{
+					if (selectedObj != null)
+					{
+						Curve entity = acadTrans.GetObject(selectedObj.ObjectId, OpenMode.ForRead) as Curve;
+						if (entity!=null)
+						{
+							if (entity.ExtensionDictionary!=ObjectId.Null)
+							{
+								using (DBDictionary dict = acadTrans.GetObject(
+									entity.ExtensionDictionary, OpenMode.ForRead, false) as DBDictionary)
+								{
+									if (dict.Contains(AppRecordKey))
+									{
+										//Получаем XRecord
+										Xrecord xrecord = acadTrans.GetObject(
+											dict.GetAt(AppRecordKey), OpenMode.ForRead) as Xrecord;
+										ResultBuffer buffer = xrecord.Data;
+										//Проходим по каждому значению в XRecord
+										foreach (TypedValue recordValue in buffer)
+										{
+											//Проверяем, была ли у нас такая группа?
+											if (GroupLenghts.ContainsKey(recordValue.Value.ToString()))
+											{
+												GroupLenghts[recordValue.Value.ToString()] += entity.GetDistanceAtParameter(entity.EndParam);
+											}
+											else
+											{
+												GroupLenghts.Add(recordValue.Value.ToString(), entity.GetDistanceAtParameter(entity.EndParam));
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			foreach (KeyValuePair<string, double> kvp in GroupLenghts)
+			{
+				acadDocument.Editor.WriteMessage("\nГруппа {0}, длина: {1}", kvp.Key, kvp.Value);
+			}
+		}
+		[CommandMethod("Proba")]
+		public void Proba()
+		{
+			Document acadDocument = Application.DocumentManager.MdiActiveDocument;
+			Database acadCurDb = acadDocument.Database;
+			using (Transaction acadTrans = acadCurDb.TransactionManager.StartTransaction())
+			{
+				//Выделять будем только линии и полилинии. Создаем фильтр
+				TypedValue[] acadFilterValues = new TypedValue[4];
+				acadFilterValues.SetValue(new TypedValue((int)DxfCode.Operator, "<OR"),0);
+				acadFilterValues.SetValue(new TypedValue((int)DxfCode.Start, "LINE"),1);
+				acadFilterValues.SetValue(new TypedValue((int)DxfCode.Start, "LWPOLYLINE"),2);
+				acadFilterValues.SetValue(new TypedValue((int)DxfCode.Operator, "OR>"),3);
+				SelectionFilter acadSelFilter = new SelectionFilter(acadFilterValues);
+				//Выделяем ВСЕ линии и полилинии на незамороженных слоях
+				SelectionSet selectionSet = acadDocument.Editor.SelectAll(acadSelFilter).Value;
+				//Проходим по получившемуся выделению
+				foreach (SelectedObject selectedObj in selectionSet)
+				{
+					if (selectedObj != null)
+					{
+						Curve entity = acadTrans.GetObject(selectedObj.ObjectId, OpenMode.ForRead) as Curve;
+						if (entity!=null)
+						{
+							acadDocument.Editor.WriteMessage("\nДлина: {0}", entity.GetDistanceAtParameter(entity.EndParam));
+						}
+					}
+				}
 			}
 		}
 	}
