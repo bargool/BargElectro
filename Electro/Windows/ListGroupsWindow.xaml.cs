5c2f10b65517ac776cd4486e5ee1dacec1d57574
bargool <bargool@gmail.com>
2011-11-03 14:31:17 +0400

    Добавил окно для списков групп

---
+++ Electro/Windows/ListGroupsWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * User: Bargool
+ * Date: 03.11.2011
+ * Time: 14:28
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace BargElectro.Windows
+{
+	/// <summary>
+	/// Interaction logic for ListGroupsWindow.xaml
+	/// </summary>
+	public partial class ListGroupsWindow : Window
+	{
+		public ListGroupsWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
